42d161a0c53567a8dcef7feddc32b3db9197b235
cppdvl <cpp.dvl@gmail.com>
2024-04-14 22:48:21 -0500

    Rogue Mode

--- ping.py
+++ ping.py
@@ -13,6 +13,8 @@ UDP_IP = sys.argv[1]
 UDP_PORT = int(sys.argv[2])
 BLOCKSIZE = int(sys.argv[3])
 LOOPBACK = sys.argv[4].lower() == "true" or sys.argv[4] == "1"
+ROGUE = sys.argv[4] == "2"
+
 
 # Sets for even and odd UIDs
 even_uids = set()
@@ -20,6 +22,13 @@ odd_uids = set()
 uid_ip_port_mapping = {}
 uid_last_sequence = {}
 uid_names = {}
+command_dict = {
+    0xdeadbee0 : "stop", #stop
+    0xdeadbee1 : "play", #play
+    0xdeadbee2 : "move", #move
+    0xdeadbeee : "ping", #ping
+    0xdeadbeef : "remove" #remove this user from uids.
+}
 
 
 
@@ -27,8 +36,7 @@ uid_names = {}
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-print(f"Router is listening at {UDP_IP}:{UDP_PORT} l:{LOOPBACK} b:{BLOCKSIZE}")
-
+print(f"Router is listening at {UDP_IP}:{UDP_PORT} l:{LOOPBACK} b:{BLOCKSIZE} rogue:{ROGUE}")
 
 def uidstr(uid):
     return str(int(uid) % 1000).zfill(3)
@@ -41,13 +49,21 @@ def reset_everything(uid):
     uid_last_sequence.clear()
     print(f"Resetting over a new host uid: {uid}")
 
+def sendtotarget(target_set, message):
+    for target_uid in target_set:
+        if target_uid not in uid_ip_port_mapping:
+            continue
+        socket.sendto(message, addr)
+
+def roguemode(uid, message):
+    target_set = (even_uids | odd_uids) - {uid}
+    sendtotarget(target_set, message)
 
 def multicast_message(uid, message):
     target_set = odd_uids if uid%2 == 0 else even_uids
-    for target_uid in target_set:
-        if target_uid in uid_ip_port_mapping:
-            target_addr = uid_ip_port_mapping[target_uid]
-            sock.sendto(message, target_addr)
+    sendtotarget(target_set, message)
+
+broadcast_message = roguemode if ROGUE == True else multicast_message
 
 def loopback_message(message, addr):
     bmessage = bytearray(message)
@@ -65,11 +81,52 @@ def printsequence(uid, sequence):
         last_uid = uid
         last_sequence = sequence
 
+def process_in_band_command(uid, sequence, message, addr):
+    print(f"Command: {command_dict[uid]}@ {sequence}, from {addr}")
+
+    if 0xdeadbee0 <= uid <= 0xdeadbee2:
+        toset = {}
+        for k,v in uid_ip_port_mapping.items():
+            if v == addr:
+                continue
+            if k in odd_uids or k in even_uids:
+                toset[k] = v
+        #broadcast the command
+        for v in toset:
+            sock.sendto(message, toset[v])
+        return
+
+    if uid == 0xdeadbeee: #an user is pinging
+        sock.sendto(message, addr)
+        return
+
+    if 0xdeadbeef: #an user is waving bye bye
+        k_uids_to_remove = [k_uid for k_uid in uid_ip_port_mapping[k_uid] == addr]
+        for k_uid in k_uids_to_remove:
+            if k_uid in even_uids:
+                even_uids = even_uids - {k_uid}
+            if k_uid in odd_uids:
+                odd_uids = odd_uids - {k_uid}
+            del uid_ip_port_mapping[k_uid]
+
+    return
+
+
+def process_loopback(uid, sequence, message, addr):
+    if len(message) == 9:
+        role = "mixer" if uid % 2 == 0 else "peer"
+        print(f"Received a test message from {role} : {uidstr(uid)} : {message}")
+        return
+
+    if last_uid != uid:
+        reset_everything(uid)
+        printsequence(uid, sequence)
+    loopback_message(message, addr)
+    return
+
 
 
 def process_message(message, addr):
-
-
 
     # Extract UID and convert it
     uid, sequence = struct.unpack('<II', message[:8])
@@ -78,46 +135,10 @@ def process_message(message, addr):
     # A streaming command is any UID in the range 0xdeadbee0 to 0xdeadbeef
 
     if 0xdeadbee0 <= uid <= 0xdeadbeef:
-        #create a dictionary indexing the command like this:
-        # 0xdeadbee0 -> "pause"
-        # 0xdeadbee1 -> "play"
-        # 0xdeadbee2 -> "move"
-        # 0xdeadbee3 -> "ping"
-        # the value should be a lambda function that takes the message and the address as arguments
-        # and returns a tuple of the new message and the new address
-        command_dict = {0xdeadbee0: "pause", 0xdeadbee1 : "play", 0xdeadbee2 : "move", 0xdeadbee3 : "ping"}
-        print(f"Command: {command_dict[uid]}@ {sequence}, from {addr}")
-
-        if uid == 0xdeadbee3:
-            sock.sendto(message, addr)
-            return
-
-        if 0xdeadbee0 <= uid <= 0xdeadbee2:
-            toset = {}
-            for k,v in uid_ip_port_mapping.items():
-                if v == addr:
-                    continue
-                if k in odd_uids or k in even_uids:
-                    toset[k] = v
-            #broadcast the command
-            for v in toset:
-                sock.sendto(message, toset[v])
-
-
-        return
-
-
-    if len(message) == 9 and LOOPBACK == True:
-        role = "mixer" if uid % 2 == 0 else "peer"
-        print(f"Received a test message from {role} : {uidstr(uid)} : {message}")
-        return;
+        return process_in_band_command(uid, sequence, message, addr)
 
     if LOOPBACK == True:
-        if last_uid != uid:
-            reset_everything(uid)
-        printsequence(uid, sequence)
-        loopback_message(message, addr)
-        return
+        return process_loopback(uid, sequence, message, addr)
 
     uidf = uidstr(uid)
     #if sequence % BLOCKSIZE != 0:
@@ -140,7 +161,7 @@ def process_message(message, addr):
     uid_last_sequence[uid] = sequence
 
     if len(odd_uids) > 0 and len(even_uids) > 0:
-        multicast_message(uid, message)
+        broadcast_message(uid, message)
     else:
         printsequence(uid, sequence)
 
@@ -148,10 +169,3 @@ while True:
     # Receive messages
     data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
     process_message(data, addr)
-
-
-
-
-
-
-
